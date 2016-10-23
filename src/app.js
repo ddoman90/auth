bc99b1616112ed30c6a4a6e60060bc6455f1ac46
ddoman90 <ddoman90@gmail.com>
2016-10-24 00:40:36 +0200

    Section 9 - Authentication with Firebase

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import firebase from 'firebase';
+
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBn_pwnulkRdR-UjiHJ4ZiYBQw9uuzzUJc",
+      authDomain: "authentication-e35ab.firebaseapp.com",
+      databaseURL: "https://authentication-e35ab.firebaseio.com",
+      storageBucket: "authentication-e35ab.appspot.com",
+      messagingSenderId: "118740226729"
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     )
   }
